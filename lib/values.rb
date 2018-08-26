b97da1959c2b38fd77e55f876748c541acba70dd
taisaoko <ttd3calvin@gmail.com>
2018-08-26 19:49:22 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -6,7 +6,7 @@ groceries = {
   vegetable: ["carrots", "broccoli", "cucumbers"],
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
-  }
+}
 
 def get_the_values(groceries)
   #code your solution here!
